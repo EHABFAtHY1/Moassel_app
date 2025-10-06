2ebc88522e7721fe5eb23fe612b79fbee18cb3d2
ehab fathy <102835566+EHABFAtHY1@users.noreply.github.com>
2025-10-06 03:31:43 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('node:path');
@@ -96,10 +94,10 @@ app.use('/api/v1/problems', problemRoutes);
 app.use('/api/v1/resources', resourceBookRoutes);
 
 
-// 404 handler for unknown routes
-// app.all('*', (req, _res, next) => {
-//   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-// });
+404 handler for unknown routes
+app.all('*', (req, _res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 app.use(globalErrorHandler);
 
 process.on('uncaughtException', (err) => {
