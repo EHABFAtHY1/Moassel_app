40725572d7f7603b45ee093b87e14abaaa94d8a2
ehab fathy <102835566+EHABFAtHY1@users.noreply.github.com>
2025-10-29 18:12:12 +0300

    Enable rate limiting middleware in app.js

--- src/app.js
+++ src/app.js
@@ -40,16 +40,16 @@ app.use(express.urlencoded({ extended: true }));
 // }));
 
 
-// app.use(
-//   '/',
-//   rateLimit({
-//     windowMs: 60 * 60 * 1000, // 1 hour
-//     max: 100,
-//     message: 'Too many requests from this IP, please try again in an hour!',
-//     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-//   })
-// );
+app.use(
+  '/',
+  rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    max: 100,
+    message: 'Too many requests from this IP, please try again in an hour!',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
 
 // CORS + cookies (قبل الراوترات)
 app.use(cookieParser());
@@ -93,7 +93,7 @@ app.use('/api/v1/resources', resourceBookRoutes);
 
 app.all('*', (req, _res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-// });
+ });
 app.use(globalErrorHandler);
 
 process.on('uncaughtException', (err) => {
