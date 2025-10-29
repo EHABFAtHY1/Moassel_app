7641cf4eb44d9d6a6aef5d00ba70bca92a563b0d
ehab fathy <102835566+EHABFAtHY1@users.noreply.github.com>
2025-10-29 17:59:26 +0300

    Uncomment rate limiting and error handling middleware

--- src/app.js
+++ src/app.js
@@ -36,21 +36,21 @@ app.use(express.urlencoded({ extended: true }));
 // }));
 
 
-// app.use(
-//   '/',
-//   rateLimit({
-//     windowMs: 60 * 60 * 1000, // 1 hour
-//     max: 100,
-//     message: 'Too many requests from this IP, please try again in an hour!',
-//     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-//   })
-// );
+app.use(
+  '/',
+  rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    max: 100,
+    message: 'Too many requests from this IP, please try again in an hour!',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
 
 // CORS + cookies (قبل الراوترات)
 app.use(cors({
   origin: '*',
-  credentials: false
+  credentials: true
 }));
 app.use(cookieParser());
 
@@ -91,15 +91,15 @@ app.use('/api/v1/resources', resourceBookRoutes);
 
 
 
-// app.all('*', (req, _res, next) => {
-//   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-// // });
-// app.use(globalErrorHandler);
+app.all('*', (req, _res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+// });
+app.use(globalErrorHandler);
 
 process.on('uncaughtException', (err) => {
   console.error('💥 Uncaught Exception!');
   console.error('Message:', err.message);        // يطبع الرسالة بس
-  // console.error('Stack:', err.stack);        // لو عايز الاستاك للتشخيص
+   console.error('Stack:', err.stack);        // لو عايز الاستاك للتشخيص
   process.exit(1); // انهي البرنامج
 });
 
